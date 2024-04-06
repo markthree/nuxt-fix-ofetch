129c04e9c460aedb3ad1aa44051ac89c3a13f2df
markthree <1801982702@qq.com>
2024-04-06 21:15:17 +0800

    fix: 修复用户端使用  onResponse 时失效, close #1

--- src/runtime/fetch.client.ts
+++ src/runtime/fetch.client.ts
@@ -45,41 +45,49 @@ export function detectResponseType(_contentType = ""): ResponseType {
 
 export default () => {
   const nullBodyResponses = new Set([101, 204, 205, 304]);
-  globalThis.$fetch = globalThis.$fetch.create({
-    async onResponse(context) {
-      if (!context.response.body) {
-        const allowParse = !nullBodyResponses.has(context.response.status) &&
-          context.options.method !== "HEAD";
-        if (allowParse) {
-          const responseType = (context.options.parseResponse
-            ? "json"
-            : context.options.responseType) ||
-            detectResponseType(
-              context.response.headers.get("content-type") || "",
-            );
-
-          // We override the `.json()` method to parse the body more securely with `destr`
-          switch (responseType) {
-            case "json": {
-              const data = await context.response.text();
-              const parseFunction = context.options.parseResponse || destr;
-              context.response._data = parseFunction(data);
-              break;
-            }
-            case "stream": {
-              context.response._data = context.response.body ||
-                // @ts-ignore Compatible with QQ Browser on iOS
-                context.response._bodyInit?.stream() ||
-                // @ts-ignore Compatible with QQ Browser on iOS
-                context.response._bodyBlob?.stream();
-              break;
-            }
-            default: {
-              context.response._data = await context.response[responseType]();
+  const _fetch = globalThis.$fetch;
+  async function $$fetch(arg1, options = {}) {
+    return await _fetch(arg1, {
+      ...options,
+      async onResponse(context) {
+        if (!context.response.body) {
+          const allowParse = !nullBodyResponses.has(context.response.status) &&
+            context.options.method !== "HEAD";
+          if (allowParse) {
+            const responseType = (context.options.parseResponse
+              ? "json"
+              : context.options.responseType) || detectResponseType(
+                context.response.headers.get("content-type") || "",
+              );
+            switch (responseType) {
+              case "json": {
+                const data = await context.response.text();
+                const parseFunction = context.options.parseResponse || destr;
+                context.response._data = parseFunction(data);
+                break;
+              }
+              case "stream": {
+                context.response._data = context.response.body || // @ts-ignore Compatible with QQ Browser on iOS
+                  context.response._bodyInit?.stream() || // @ts-ignore Compatible with QQ Browser on iOS
+                  context.response._bodyBlob?.stream();
+                break;
+              }
+              default: {
+                context.response._data = await context.response[responseType]();
+              }
             }
           }
         }
-      }
-    },
+        if (options.onResponse) {
+          return await options.onResponse(context);
+        }
+      },
+    });
+  }
+
+  globalThis.$fetch = $$fetch.bind(_fetch);
+
+  Object.keys(_fetch).forEach((key) => {
+    globalThis.$fetch[key] = _fetch[key];
   });
 };
